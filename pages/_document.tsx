9ccfa8bf7e5ced85023971939ef31897bf8030e5
Dmytro Honcharenko <dimeeentor@gmail.com>
2023-04-09 14:21:37 +0200

    made small changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <title>Apple Clone</title>
-            </Head>
+            <Head />
+            <title>Apple Clone</title>
             <body>
                 <Main />
                 <NextScript />
